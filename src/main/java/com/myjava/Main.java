405152861f1f9e2326ea153e4b2a662a3bcee115
javierdimastri <hilmantrihatmojo@gmail.com>
2024-08-28 21:25:57 +0700

    [hilman] add initial

--- src/main/java/com/myjava/Main.java
+++ src/main/java/com/myjava/Main.java
@@ -1,7 +1,13 @@
 package com.myjava;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    private int x;
+
+    public int getX() {
+        return x;
+    }
+
+    public int setX(int x) {
+        return this.x = x;
     }
 }
---
+++ src/main/java/com/myjava/object/Second.java
@@ -0,0 +1,12 @@
+package com.myjava.object;
+
+import com.myjava.Main;
+
+public class Second {
+  public static void main(String[] args) {
+    Main myMain = new Main();
+    myMain.setX(5);
+
+    System.out.println(myMain.getX());    
+  }
+}
